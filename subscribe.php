8cd12f8ecaadbe2a46ff44c5804e033251dda7cb
MuhdNihalCY <mnihalcy@gmail.com>
2024-03-21 18:13:30 +0530

    Updates the code. changed namming some nammings.

--- subscribe.php
+++ subscribe.php
@@ -1,6 +1,6 @@
 <?php 
 /*
-Plugin Name: Subscriptionss
+Plugin Name: Social links
 Plugin URI: https://example.com
 Description: A very basic test plugin for subscriptions link
 Version: 1.0
@@ -8,21 +8,25 @@ Author: Nihal
 Author URI: https://example.com
 License: GPL2
 */ 
-function subscribe_link_att($atts) {
+
+function social_link_att($atts) {
     $default = array(
         'link' => '#',
         'title' => '#',
     );
     $a = shortcode_atts($default, $atts);
-    return '<a rel="nofollow" class="subs-link" href="'.$a["link"].'">'.$a["title"].'</a>';
+    return '<a rel="nofollow" class="social-link" href="' . $a["link"] . '">' . $a["title"] . '</a>';
 }
-add_shortcode('subscribe', 'subscribe_link_att');
+add_shortcode('social', 'social_link_att');
 
 function enqueue_custom_styles() {
     // Get the URL of the plugin directory
-    $dir_url = plugins_url('style.css',__FILE__);
+    $dir_url = plugins_url('style.css', __FILE__);
 
     // Enqueue your custom stylesheet
     wp_enqueue_style('style', $dir_url);
 }
+
+// Ensure this line is present to call the function
 add_action('wp_enqueue_scripts', 'enqueue_custom_styles');
+
